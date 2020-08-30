c2fad605599e3e006938d5e206c83073af1c91f3
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-30 16:32:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 
 def countdown(seconds)
-seconds = get.chomp
- while seconds > 0
-  puts "#{seconds} SECOND(S)!"
+seconds = gets.chomp
+number = seconds.to_i
+ while number > 0
+  puts "#{number} SECOND(S)!"
   sleep(1)
-  econds -= 1
+  number -= 1
   end
   return "Happy New Year"
 end
