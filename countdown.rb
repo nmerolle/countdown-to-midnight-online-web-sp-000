b868a8bac74518f12e2a2dafce622389a20034fb
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-30 16:29:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,11 @@
 #write your code here
-
+seconds = 10
 def countdown(seconds)
-
+ while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  sleep(1)
+  econds -= 1
+  end
+  return "Happy New Year"
+end
 end
